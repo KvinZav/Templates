c7b2f77f989c7a3deb27a2bc4df7664e227f52a5
KevinZav <kevin.zavala@umvel.com>
2023-01-06 12:03:02 -0600

    fix: change date format

--- server.js
+++ server.js
@@ -4,20 +4,12 @@ const hbs = require('express-handlebars');
 const path = require('path');
 const moment = require('moment');
 
-
-
 const app = express();
 const port = process.env.PORT || 3000;
 const exphbs = hbs.create({
   layoutsDir: false,
   extname: '.hbs',
-  helpers: {
-    dateFormat(date, format) {
-      return moment(date).format(format);
-    }
-  }
 });
-
 
 app.set('port', port);
 app.set('views', path.join(__dirname, 'views'));
@@ -27,13 +19,15 @@ app.set('view engine', '.hbs');
 app.use(express.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-
 app.get('/', (_, res) => {
   res.render('email-publication', {
     layout: false,
     brandName: 'Cupcake Extravaganza',
-    bussinessOwnerName: 'Genea',
-    publicationDateTime: '2022-10-20T17:30:00.000Z',
+    businessOwnerName: 'Genea',
+    publicationDateTime: {
+      date: moment('2022-10-20T17:30.000Z').format('LL'),
+      hour: moment('2022-10-20T17:30.000Z').format('hh:mm a'),
+    },
     publicationName: 'Christmas launch',
     link: 'https://www.google.com',
   });
